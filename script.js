8e155ee3880ed5b3461dcdb2a90b42322f7e1c4e
muhamaddafa <gaming102dafa@gmail.com>
2021-07-22 17:19:49 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// menulis java script
+
+/* var x = 10;
+console.log("Hello World");
+console.log("Isi dari variabel x adalah " + x);
+*/
+
+/* akhir menulis javascrpit */
+
+// Pop Up Box
+
+// Alert
+/* alert("Hello World");
+alert("Halo");
+alert("Nama saya");
+alert("Muhamad Dafa"); */
+
+// Prompt
+/* var nama = prompt("Masukan Nama");
+alert("Halo " + nama + " Selamat Pagi"); */
+
+// Confirm
+/* var tes = confirm("Apakah Kamu Yakin??");
+if (tes === true) {
+  alert("OKE TERIMAKASIH");
+} else {
+  alert("Oke anjing");
+} */
+
+/* Akhir Pop Up Box */
+
+// Control Flow (Pengulangan & Pengkondisian)
+
+// Pengulangan (WHILE)
+/* var ulang = true;
+while (ulang) {
+  console.log("Muhamad Dafa");
+  ulang = confirm("Lagi?");
+} */
+
+/* var nilaiAwal = 1;
+while (nilaiAwal <= 10) {
+  console.log("muhamad Dafa");
+  nilaiAwal++;
+} */
+
+var nama = prompt("Masukkan Nama Anda :");
+alert("Halo " + nama + " Apakah Kamu Siswa 12 Mipa 7");
